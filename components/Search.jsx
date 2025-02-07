cbb9468de2abe3dff822f4562075e4f472ef2d06
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2025-02-07 15:35:39 +0500

    update search layout

--- components/Search.jsx
+++ components/Search.jsx
@@ -11,38 +11,29 @@ const Search = () => {
   return (
     <div>
       <div className="relative xl:w-2/6 lg:w-3/4 mx-auto ">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg
-            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
+        <div class="flex rounded-full border-2 border-blue-500 overflow-hidden max-w-md mx-auto font-[sans-serif]">
+          <input
+            type="search"
+            id="default-search"
+            placeholder="Search with sport name"
+            value={searchTerm}
+            className="w-full outline-none bg-white text-sm px-5 py-4"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button
+            type="button"
+            class="flex items-center justify-center bg-blue-500 hover:bg-blue-600 px-6"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 192.904 192.904"
+              width="18px"
+              class="fill-white"
+            >
+              <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
+            </svg>
+          </button>
         </div>
-        <input
-          type="search"
-          id="default-search"
-          className="block w-full p-4 sm:p-8 ps-10 text-sm sm:text-[20px] tracking-wider text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Enter sport name"
-          required
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="text-white tracking-widest   absolute end-2.5 bottom-2.5 sm:bottom-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg   px-4 py-1 sm:py-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button>
       </div>
 
       {posts ? <Posts posts={posts} searchTerm={searchTerm} /> : null}
